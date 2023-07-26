1234e129e55ab6d98d60ba8984bc80f6c6acf22b
Rafael Augusto da Silva <rafaelaugustodasilva2@gmail.com>
2023-07-26 15:20:08 -0300

    :sparkles: feat: Create Tutor Route

--- src/controllers/tutor.controller.ts
+++ src/controllers/tutor.controller.ts
@@ -8,6 +8,13 @@ class TutorController {
     const tutors = await TutorService.getAllTutors();
     res.status(StatusCodes.OK).json(tutors);
   }
+  async createTutor(req: Request, res: Response) {
+    // validateTutorDataCreate(tutorData);
+
+    const tutorData = req.body;
+    const newTutor = await TutorService.createTutor(tutorData);
+    res.status(StatusCodes.CREATED).json(newTutor);
+  }
 }
 
 export default new TutorController();
--- src/routes/Tutor.routes.ts
+++ src/routes/Tutor.routes.ts
@@ -4,5 +4,6 @@ import TutorController from '../controllers/tutor.controller';
 const router = express.Router();
 
 router.get('/tutors', TutorController.getAllTutors);
+router.post('/tutor', TutorController.createTutor);
 
 export default router;
