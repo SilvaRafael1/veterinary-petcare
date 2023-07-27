87deaf54a64867987d7c3ac33e4fd93c40404b79
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-07-27 15:35:37 -0300

    ✨ feat: Add UPDATE Pet Validator

--- src/routes/Pet.routes.ts
+++ src/routes/Pet.routes.ts
@@ -3,9 +3,15 @@ import PetController from '../controllers/pet.controller';
 import authenticateUser from '../middleware/authentication';
 import validator from '../middleware/validator';
 import { createPetSchema } from '../schemas/Pet/createPet';
+import { updatePetSchema } from '../schemas/Pet/updatePet';
 const router = express.Router();
 
 router.post('/:tutorId', authenticateUser, validator(createPetSchema), PetController.createPet);
-router.put('/:petId/tutor/:tutorId', authenticateUser, PetController.updatePet);
+router.put(
+  '/:petId/tutor/:tutorId',
+  authenticateUser,
+  validator(updatePetSchema),
+  PetController.updatePet,
+);
 router.delete('/:petId/tutor/:tutorId', authenticateUser, PetController.deletePet);
 export default router;
